546f1d824984e814dca305d6f13095c3cafa3d20
THIAGO MOYSES <thiago-_-moyses@hotmail.com>
2018-10-03 22:42:18 -0300

    Add files via upload

--- codigos/socket/cliente.py
+++ codigos/socket/cliente.py
@@ -5,16 +5,15 @@ print("#   CLIENTE    #")
 print("################")
 
 ##ip = input('digite o ip de conexao: ') 
-ip = "192.168.200.203"
-port = 7000 
-addr = ((ip,port)) 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-teste = client_socket.connect(addr) 
-
-
-mensagem = input("-> ")
-##msg = byte(mensagem)
-msg = bytes(mensagem, 'utf-8')
-client_socket.send(msg) 
-print ("mensagem enviada") 
-client_socket.close()
+while 1:
+	ip = "192.168.0.100"
+	port = 7000
+	addr = ((ip,port))
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client_socket.connect(addr)
+	mensagem = input("-> ")
+	##msg = byte(mensagem)
+	msg = bytes(mensagem, 'utf-8')
+	client_socket.send(msg) 	
+	print ("mensagem enviada")
+	client_socket.close()
